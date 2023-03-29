259cfbbf63ba5875812dfe172a5c0e96bfe6a320
sasikumar-07 <128038363+sasikumar-07@users.noreply.github.com>
2023-03-29 14:32:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MenuItem from './MenuItem';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+ <MenuItem/>
+    </div>
+  );
+}
+
+export default App;
